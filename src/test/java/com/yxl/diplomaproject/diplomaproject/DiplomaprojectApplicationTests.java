6e3cbad56fba58e5b020e76b0045a384154e0fd9
yxl291 <m17602287670@163.com>
2019-04-24 11:22:09 +0800

    创建老年健康知识学习系统

---
+++ src/test/java/com/yxl/diplomaproject/diplomaproject/DiplomaprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yxl.diplomaproject.diplomaproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiplomaprojectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
