c6bb83be5e414c49f24aa43e2c97e7f5b3dbf3c8
yxl291 <m17602287670@163.com>
2019-04-29 16:53:00 +0800

    后台用户管理的增删

---
+++ src/test/java/com/yxl/diplomaproject/DiplomaprojectApplicationTests.java
@@ -0,0 +1,14 @@
+package com.yxl.diplomaproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class DiplomaprojectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
